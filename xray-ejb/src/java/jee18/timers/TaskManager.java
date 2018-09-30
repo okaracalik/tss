b4a07f0ff15452fd545e94f76f21d7f05d4f4c4c
Orkut Karaçalık <orkutkaracalik@hotmail.com>
2018-09-30 18:56:35 +0200

    rename package

--- xray-ejb/src/java/jee18/timers/TaskManager.java
+++ xray-ejb/src/java/jee18/timers/TaskManager.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package timers;
+package jee18.timers;
 
 import java.time.LocalDate;
 import java.util.Arrays;
